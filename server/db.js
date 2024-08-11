84b6908b4e12304a491712558d3cab76ae4dad05
jdawsonCR <jdawson@criticalreach.org>
2024-08-11 14:36:39 -0400

    build failed

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:MTQ0MzUtamVubGJv@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:MTY1MTQtamVubGJv@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const API_URL = window.location.hostname === "localhost" ? "https://jenlbower-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" : "https://jenlbower-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/";
 console.log(
-    "API_URL :",
+    "API_URL : http://localhost:8181",
     API_URL
 );
